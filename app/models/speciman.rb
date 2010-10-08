a6fdecc58807fb49762f6df8f4ccd50efb4c593e
Andrey Ryzhkov <rizkoff@mail.ru>
2010-10-09 01:36:22 +0400

    - restored ordering

--- app/models/speciman.rb
+++ app/models/speciman.rb
@@ -17,7 +17,7 @@ class Speciman < ActiveRecord::Base
 
   before_validation :upcase_title
 
-  # default_scope :order => 'priord DESC, title_upcased ASC'
+  default_scope :order => 'priord DESC, title_upcased ASC'
 
   def self.search(search)
     where("title_upcased LIKE '%#{upcase_ru(search)}%'")
