8321cf0ec1ec6de4953b79a3fdc67f2c2eb3e180
Andrey Ryzhkov <rizkoff@mail.ru>
2010-10-09 01:04:17 +0400

    - db parms moved from initializer to model

--- app/models/speciman.rb
+++ app/models/speciman.rb
@@ -1,4 +1,16 @@
 class Speciman < ActiveRecord::Base
+  def self.abc
+    Prm.find_by_title('abc.ru').pval
+  end
+
+  def self.abccap
+    Prm.find_by_title('ABC.ru').pval
+  end
+
+  def self.upcase_ru(s)
+    (s.blank?)? '' : s.tr(abc, abccap)
+  end
+
   has_many :shipments, :dependent => :restrict
   has_many :factors, :dependent => :restrict
   has_many :fvalues, :through => [:shipments, :factors]
@@ -8,13 +20,14 @@ class Speciman < ActiveRecord::Base
   default_scope :order => 'priord DESC, title_upcased ASC'
 
   def self.search(search)
-    where("title_upcased LIKE '%#{search.try(:upcase_ru)}%'")
+    where("title_upcased LIKE '%#{upcase_ru(search)}%'")
   end
 
   private
 
     def upcase_title
-      self.title_upcased = (title.blank?)? nil : title.upcase_ru
+      self.title_upcased = upcase_ru(title)
+      # self.title_upcased = (title.blank?)? nil : upcase_ru(title)
     end
 
 end
--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,17 +1,1 @@
 # coding: utf-8
-class String
-  @@abc    ||= Prm.find_by_title('abc.ru').pval
-  @@abccap ||= Prm.find_by_title('ABC.ru').pval
-
-  def self.abccap
-    @@abccap
-  end
-  def self.abc
-    @@abc
-  end
-
-  def upcase_ru
-    tr @@abc, @@abccap
-  end
-
-end
